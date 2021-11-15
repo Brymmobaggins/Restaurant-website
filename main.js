c51a3b0786174e31abf0376438ce945b6fc2c266
Brymmobaggins <ibro2tablet21@yahoo.com>
2021-11-15 17:48:43 +0100

    add comment on unwanted code

--- main.js
+++ main.js
@@ -3,16 +3,21 @@ const form = document.getElementById("form")
 form.addEventListener('submit', function (e) {
     e.preventDefault(); // to stop form from submitting
     let customerData = {
-        ID: document.getElementById('cus-id').value,
         Name: document.getElementById('cus-name').value,
-        pick: document.getElementById('pick-loc').value,
-        drop: document.getElementById('drop-loc').value
+        ID: document.getElementById('cus-id').value,
+        Pick: document.getElementById('pick-loc').value,
+        Drop: document.getElementById('drop-loc').value
     }
-    // saving  Customer data local Storage
-    localStorage.setItem('customerData', JSON.stringify(customerData))
+    document.forms[0].reset() // to clear the form for next entries
+    // for display purpose
+    // let pre = document.querySelector('msg pre')
+    // pre.textContent = '\n' + JSON.stringify(customerDatas, '\t', 2)
+
+    // Saving Customer data in local Storage
+    localStorage.setItem('customerDetails', JSON.stringify(customerData))
     console.log(localStorage.getItem(customerData))
 
-    // saving customer info insie an array
+    // saving customer info inside an array
     // customers.push(customerInfo)
     // let json = JSON.stringify(customerInfo);
     // localStorage.setItem(username,json)
