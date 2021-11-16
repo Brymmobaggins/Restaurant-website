352248f9c5dd93426f8d167b4e7902473e4e41da
Brymmobaggins <ibro2tablet21@yahoo.com>
2021-11-16 12:31:19 +0100

    modify js code

--- main.js
+++ main.js
@@ -9,19 +9,10 @@ form.addEventListener('submit', function (e) {
         Drop: document.getElementById('drop-loc').value
     }
     document.forms[0].reset() // to clear the form for next entries
-    // for display purpose
-    // let pre = document.querySelector('msg pre')
-    // pre.textContent = '\n' + JSON.stringify(customerDatas, '\t', 2)
 
     // Saving Customer data in local Storage
     localStorage.setItem('customerDetails', JSON.stringify(customerData))
     console.log(localStorage.getItem(customerData))
-
-    // saving customer info inside an array
-    // customers.push(customerInfo)
-    // let json = JSON.stringify(customerInfo);
-    // localStorage.setItem(username,json)
-
 
 })
 // function showDate(slot1, slot2, slot3, slot4) {
