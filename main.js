9e9c9506f3c7afd1eb41fd05480cd4e8c0ac73e2
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-04-06 12:54:42 +0100

    fix contact and menu section

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 let menuBtn = document.getElementById("menu-btn")
 let menu = document.getElementById("menu")
-let closeMenu = document.querySelector("span")
+let closeMenu = document.getElementsByTagName("span")[0]
 let contactBtn = document.getElementById("contact-btn")
 let contact = document.getElementById("contact")
+let closeContact = document.getElementsByTagName("span")[1]
 // let 
 
 menuBtn.addEventListener("click", function () {
     menu.style.display = "block"
+    contact.style.display = "none"
 
 })
-
 closeMenu.addEventListener("click", function () {
     menu.style.display = "none"
 })
-contactBtn.addEventListener("click", function(){
+contactBtn.addEventListener("click", function () {
+    contact.style.display = "block"
 
+})
+closeContact.addEventListener("click", function () {
+    contact.style.display = "none"
 })
