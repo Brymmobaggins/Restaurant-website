41a7cb4e35cc05bc2edd2b52333f04e5d32a21e0
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-04-09 22:31:07 +0100

    add text for store resumption

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
+
+
 let menuBtn = document.getElementById("menu-btn")
 let menu = document.getElementById("menu")
 let closeMenu = document.getElementsByTagName("span")[0]
 let contactBtn = document.getElementById("contact-btn")
 let contact = document.getElementById("contact")
 let closeContact = document.getElementsByTagName("span")[1]
-// let 
 
 menuBtn.addEventListener("click", function () {
     menu.style.display = "block"
